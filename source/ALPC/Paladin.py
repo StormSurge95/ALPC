b17031217140f8cb117f5d15112f40958b5e8d9a
StormSurge95 <strmcassidy@gmail.com>
2022-05-01 21:13:12 -0500

    added Paladin.py

---
+++ source/ALPC/Paladin.py
@@ -0,0 +1,91 @@
+from .Tools import Tools
+from .Constants import Constants
+from .PingCompensatedCharacter import PingCompensatedCharacter
+import asyncio
+import re
+
+class Paladin(PingCompensatedCharacter):
+    ctype = 'paladin'
+
+    async def manaShieldOff(self):
+        async def msOffFn():
+            nonlocal self
+            if not self.ready: raise Exception("We aren't ready yet [manaShieldOff].")
+            if not Tools.hasKey(self.s, 'mshield'): return False # it's already off
+
+            unshield = asyncio.get_event_loop().create_future()
+            def reject(reason = None):
+                nonlocal self, unshield
+                if not unshield.done():
+                    self.socket.off('player', successCheck)
+                    unshield.set_exception(Exception(reason))
+            def resolve(value = None):
+                nonlocal self, unshield
+                if not unshield.done():
+                    self.socket.off('player', successCheck)
+                    unshield.set_result(value)
+            def successCheck(data):
+                if not Tools.hasKey(data, 's') or not Tools.hasKey(data.s, 'mshield'):
+                    resolve(True)
+            Tools.setTimeout(reject, Constants.TIMEOUT, f"manaShieldOff timeout ({Constants.TIMEOUT}s)")
+            self.socket.on('player', successCheck)
+            await self.socket.emit('skill', { 'name': 'mshield' })
+            while not unshield.done():
+                await asyncio.sleep(Constants.WAIT)
+            return unshield.result()
+        return await Tools.tryExcept(msOffFn)
+    
+    async def manaShieldOn(self):
+        async def msOnFn():
+            nonlocal self
+            if not self.ready: raise Exception("We aren't ready yet [manaShieldOn].")
+            if Tools.hasKey(self.s, 'mshield'): return False
+
+            shielded = asyncio.get_event_loop().create_future()
+            def reject(reason = None):
+                nonlocal self, shielded
+                if not shielded.done():
+                    self.socket.off('player', successCheck)
+                    shielded.set_exception(Exception(reason))
+            def resolve(value = None):
+                nonlocal self, shielded
+                if not shielded.done():
+                    self.socket.off('player', successCheck)
+                    shielded.set_result(value)
+            def successCheck(data):
+                if data.get('s', {}).get('mshield'):
+                    resolve(True)
+            Tools.setTimeout(reject, Constants.TIMEOUT, f"manaShieldOn timeout ({Constants.TIMEOUT}s)")
+            self.socket.on('player', successCheck)
+            await self.socket.emit('skill', { 'name': 'mshield' })
+            while not shielded.done():
+                await asyncio.sleep(Constants.WAIT)
+            return shielded.result()
+        return await Tools.tryExcept(msOnFn)
+    
+    async def selfHeal(self):
+        async def healFn():
+            nonlocal self
+            if not self.ready: raise Exception("We aren't ready yet [selfHeal].")
+            healed = asyncio.get_event_loop().create_future()
+            def reject(reason = None):
+                nonlocal self, healed
+                if not healed.done():
+                    self.socket.off('eval', cooldownCheck)
+                    healed.set_exception(Exception(reason))
+            def resolve(value = None):
+                nonlocal self, healed
+                if not healed.done():
+                    self.socket.off('eval', cooldownCheck)
+                    healed.set_result(value)
+            def cooldownCheck(data):
+                match = re.search('skill_timeout\s*\(\s*[\'"]selfheal[\'"]\s*,?\s*(\d+\.?\d+?)?\s*\)', data['code'])
+                if match != None:
+                    resolve(True)
+            Tools.setTimeout(reject, Constants.TIMEOUT, f"selfheal timeout ({Constants.TIMEOUT}s)")
+            self.socket.on('eval', cooldownCheck)
+            await self.socket.emit('skill', { 'name': 'selfheal' })
+            while not healed.done():
+                await asyncio.sleep(Constants.WAIT)
+            return healed.result()
+        return await Tools.tryExcept(healFn)
--- source/ALPC/__init__.py
+++ source/ALPC/__init__.py
@@ -4,10 +4,12 @@ from .Entity import Entity
 from .Game import Game
 from .Mage import Mage
 from .Observer import Observer
+from .Paladin import Paladin
 from .Pathfinder import Pathfinder
 from .PingCompensatedCharacter import PingCompensatedCharacter
 from .Player import Player
 from .Tools import Tools
 
 __all__ = [ 'Character', 'Constants', 'Entity', 'Game', 'Mage', 'Observer',
-            'Pathfinder', 'PingCompensatedCharacter', 'Player', 'Tools' ]
+            'Paladin', 'Pathfinder', 'PingCompensatedCharacter', 'Player',
+            'Tools' ]
