a0018a6712909d3199d3fe42f646cc5f6dc63b0c
StormSurge95 <strmcassidy@gmail.com>
2022-05-23 06:04:31 -0500

    test pathfinder

--- source/Test.py
+++ source/Test.py
@@ -8,17 +8,24 @@ import ALPC as AL
 logging.root.setLevel(logging.DEBUG)
 
 async def main():
-    async with aiohttp.ClientSession() as session:
-        await AL.Game.loginJSONFile(session, '.\credentials.json')
-        await AL.Game.getGData(session, True, True)
-        AL.Pathfinder.G = AL.Game.G
-        await AL.Pathfinder.prepare(AL.Game.G)
-        observers = [await AL.Game.startObserver(session, 'US', 'I'), await AL.Game.startObserver(session, 'US', 'II'), await AL.Game.startObserver(session, 'US', 'III'), 
-                    await AL.Game.startObserver(session, 'EU', 'I'), await AL.Game.startObserver(session, 'EU', 'II'), await AL.Game.startObserver(session, 'ASIA', 'I')]
-        char = await AL.Game.startCharacter(session, 'StormSurge', 'US', 'III', False)
-        for obs in observers:
-            await obs.socket.disconnect()
-        await char.disconnect()
+    if __name__ == '__main__':
+        async with aiohttp.ClientSession() as session:
+            await AL.Game.loginJSONFile(session, '.\credentials.json')
+            await AL.Game.getGData(session, True, True)
+            AL.Pathfinder.G = AL.Game.G
+            await AL.Pathfinder.prepare(AL.Game.G)
+            observers = [await AL.Game.startObserver(session, 'US', 'I'), await AL.Game.startObserver(session, 'US', 'II'), await AL.Game.startObserver(session, 'US', 'III'), 
+                        await AL.Game.startObserver(session, 'EU', 'I'), await AL.Game.startObserver(session, 'EU', 'II'), await AL.Game.startObserver(session, 'ASIA', 'I')]
+            # char = await AL.Game.startCharacter(session, 'StormSurge', 'US', 'III', False)
+            # await char.smartMove('main')
+            # await char.smartMove('halloween')
+            # await char.smartMove('winterland')
+            # await char.smartMove('desertland')
+            # await char.smartMove('main')
+            await asyncio.sleep(30)
+            for obs in observers:
+                await obs.socket.disconnect()
+            # await char.disconnect()
 
 if sys.platform == 'win32':
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
